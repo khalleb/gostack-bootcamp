88c91ada0eef013fee93e33dbecb6d0cdc119191
khalleb <khallebw@gmail.com>
2020-08-25 21:17:43 -0300

    Finalizado conceitos React Native

---
+++ 03 - Mobile com React Native/mobile/src/index.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import {
+  SafeAreaView,
+  Text,
+  FlatList,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+} from "react-native";
+
+import api from "./services/api";
+
+export default function App() {
+  const [repositories, setRepositories] = useState();
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+  async function handleAddRepository() {
+    const response = await api.post("repositories", {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Khalleb Ribeiro",
+    });
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={repositories}
+          keyExtractor={(repository) => repository.id}
+          renderItem={({ item: repository }) => (
+            <Text style={styles.repository}>{repository.title}</Text>
+          )}
+        />
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.button}
+          onPress={handleAddRepository}
+        >
+          <Text style={styles.buttonText}>Adicionar Projeto</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#7159c1",
+  },
+  repository: {
+    color: "#FFF",
+    fontSize: 20,
+  },
+  button: {
+    backgroundColor: "#FFF",
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
